12b1a97113ad098cc99fa3dcb893226fc951cc7d
davidffh <davidffh@gmail.com>
2021-06-16 12:23:26 +0000

    First commit 06

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -8,7 +8,7 @@ sap.ui.define([
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {typeof sap.ui.model.Filter} Filter
      * @param {typeof sap.ui.model.FilterOperator} FilterOperator
-     * 
+     *  
      */
     function (Controller, Filter, FilterOperator) {
         "use strict";
@@ -79,6 +79,14 @@ sap.ui.define([
             oModel.setProperty("/CountryKey", "");
         }
 
+        function showPostalCode(oEvent) {
+           var itemPressed = oEvent.getSource();//item pulsado
+           var oContext = itemPressed.getBindingContext();
+           var objectContext = oContext.getObject();
+
+           sap.m.MessageToast.show(objectContext.PostalCode);
+        }
+
         var Main = Controller.extend("logaligroup.Employees.controller.MainView", {});
 
         Main.prototype.onValidate = function () {
@@ -100,6 +108,7 @@ sap.ui.define([
         Main.prototype.onInit = onInit;
         Main.prototype.onFilter = onFilter;
         Main.prototype.onClearFilter = onClearFilter;
+        Main.prototype.showPostalCode = showPostalCode;
         
         return Main;
     });
